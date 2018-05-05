adb4f6219293c041548b334c92d7b3857f163b9f
Archimedes <19399779+PJP2810@users.noreply.github.com>
2018-05-05 15:21:04 +0100

    Creating segmenter for `cached_definitions.json`
    Set up segmenting function for `cached_definitions.json`
    Output each main branch of JSON as its own json, into `\cached_definitions\` directory
    Output non-iterable values from `cached_definitions.json` as strings to their respective files in `\cached_definitions\` directory

--- cached_definitionsSegmenter.py
+++ cached_definitionsSegmenter.py
@@ -9,24 +9,44 @@ import os
 # Move to workingDirectory
 os.chdir("workingDir")
 
+# Separate JSON segments
+def writeSeg(fullData, segment):
+    
+    # Move to output directory
+    os.chdir("cached_definitions")
+    
+    try:
+        if isinstance(data[i], int) or isinstance(data[i], bool):
+            with open(i+".txt", "w") as outfile:
+                outfile.write(i + ":" + str(data[i]))
+        else:
+            segData = [x for x in fullData[str(segment)]]
+            with open(str(segment)+'.json', 'w') as outfile:
+                json.dump(segData, outfile, indent=True,sort_keys=True)
+    except:
+        print("---Exception--- " + str(data[i]) +"\n")
+    
+    # Return to workingDirectory
+    os.chdir("..")
+    
+    return
+
+'''
+main_
+'''
+
 # Read JSON file
 with open('cached_definitions.json') as json_file:  
     data = json.load(json_file)
 
-# Separate JSON segments
-def segJSON(fullData, segment):
-    
-    segData = [x for x in fullData[str(segment)]]
-    
-    with open(str(segment)+'.json', 'w') as outfile:
-        json.dump(segData, outfile, indent=True,sort_keys=True)
-    return
+# Create list of desired segments
+segments = ['hero_defines', 'upgrade_defines', 'loot_defines']
 
+# Iterate through segments
+for i in data:
+    print(i)
+    writeSeg(data, i)
 
-
-segment = 'loot_defines'
-
-segJSON(data, segment)
 
 '''
 TODO:
